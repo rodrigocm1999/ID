779a9c1fdfeaec3eaffab609788e3089351b944e
PatinhoGamer <40546524+PatinhoGamer@users.noreply.github.com>
2018-11-30 23:37:08 +0000

    dsa

--- src/trabalho/Camaras.java
+++ src/trabalho/Camaras.java
@@ -5,23 +5,23 @@
  */
 package trabalho;
 
+import java.io.File;
 import java.util.ArrayList;
+import org.jdom2.Attribute;
 import org.jdom2.Document;
 import org.jdom2.Element;
+import org.jdom2.Namespace;
 
 /**
  *
  * @author Rodrigo
  */
 public class Camaras {
-    
+
     public static String[] camaraStrings = new String[]{"nome", "area", "nHabitantes", "nFreguesias", "feriado", "presidente",
-         "assembleia", "telefone", "email", "site", "brasao", "morada", "codPostal"};
+        "assembleia", "telefone", "email", "site", "brasao", "morada", "codPostal"};
     private static String regExp = "<div class=\"sel3\">(?<nome>.*)<\\/div><br><div class=\"f3\">.*área de (?<area>\\d*),\\d km2, (?<nHabitantes>\\d+ \\d+).* (?<nFreguesias>\\d+) freguesias.*f3>(?<distrito>.*)<\\/a>.*feriado municipal, (?<feriado>\\d{4}-\\d\\d-\\d\\d).*\"f3\">(?<presidente>.*),.*\\r\\n.*>(?<assembleia>.*)\\s?,.*\\r\\n.*sel2\">(?<morada>.*)<br> (?<codPostal>\\d+-\\d+).*<\\/.*Telefone:.(?<telefone>.*) <br>.*\\r\\n.*mailto:(?<email>.*)\" .*\\r\\n.*<div class=\"f1\" align=\"left\"><a href=\"(?<site>.*\")\\sclass=\"f2\".*\\r\\n.*SRC=\"(?<brasao>.*)\" A";
 
-    
-    
-    
     public static void Run() {
         // Link:    https://www.anmp.pt/anmp/pro/mun1/mun101w3.php?cod=M3000
         /*Minicipios de coimbra
@@ -47,7 +47,6 @@ public class Camaras {
         }
         for (int i = 0; i < threadCamaras.size(); i++) {
             try {
-
                 thread = threadCamaras.get(i);
                 thread.join();
                 Element element = thread.getElement();
@@ -59,8 +58,20 @@ public class Camaras {
             }
         }
         XMLfunc.escreverDocumentoParaFicheiro(doc, "camaras.xml");
-        
-        JDOMFunc_Validar.validarXSD("camaras");
+        try {
+            File f = new File("camaras.xsd");
+            if (f.exists()) {
+                Namespace XSI = Namespace.getNamespace("xsi", "http://www.w3.org/2001/XMLSchemainstance");
+                municipios.addNamespaceDeclaration(XSI);
+                municipios.setAttribute(new Attribute("noNamespaceSchemaLocation", "camaras.xsd",
+                        Namespace.getNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance")));
+                JDOMFunc_Validar.validarXSD("camaras.xml");
+            } else {
+                System.out.println("O ficheiro camaras.xsd não existe");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
     public static String getRegExp() {
