5a85be6f5440044192a15310f3b7383d36c6e58d
PatinhoGamer <40546524+PatinhoGamer@users.noreply.github.com>
2018-12-05 21:29:07 +0000

    s

--- src/trabalho/Main.java
+++ src/trabalho/Main.java
@@ -19,43 +19,43 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception {
-        
+
         MainWindow main = new MainWindow();
         main.show();
-        
-        Camaras.Run();
+
+        //juntar2ficheiros();
+        //Camaras.Run();
         //Contratos.Run();
     }
 
-    
-    public static void juntar2ficheiros(){
-        
+    public static void juntar2ficheiros() {
+
         Document docTudo = Util.lerDocumentoXML("camaras.xml");
         Document docContratos = Util.lerDocumentoXML("contratos.xml");
-        
-        
+
         Element mun = docTudo.getRootElement();
-        List<Element> list =  mun.getChildren();
-        
-        /*Element mun = docContratos.getRootElement();
-        List<Element> list =  mun.getChildren();*/
-        
-        
-        
-        for (int i = 0; i < list.size(); i++) {
-            Element el = list.get(i);
-            
-            
-            
+        List<Element> municipioslist = mun.getChildren();
+
+        Element cont = docContratos.getRootElement();
+        List<Element> munList = cont.getChildren();
+
+        for (int i = 0; i < municipioslist.size(); i++) {
+            Element munElement = municipioslist.get(i);
+            String nomeMun = munElement.getChildText("nome");
+
+            for (int j = 0; j < munList.size(); j++) {
+                Element contElement = munList.get(j);
+                String str = contElement.getAttributeValue("nomeMun");
+
+                if (nomeMun.equals(nomeMun)) {
+                    contElement.detach();
+                    munElement.addContent(contElement);
+                    break;
+                }
+            }
         }
-        
-        
-        
-        //municipios.getChild()
-        
-        
-        
-        
+
+        Util.escreverDocumentoParaFicheiro(docTudo, "tudo.xml");
     }
-    
+
 }
