b59dc305c704ef3a7487247eb47a33149afa465b
PatinhoGamer <40546524+PatinhoGamer@users.noreply.github.com>
2019-01-04 18:44:53 +0000

    swda

--- src/trabalho/Contrato.java
+++ src/trabalho/Contrato.java
@@ -5,29 +5,36 @@ import org.jdom2.Element;
 
 
 public class Contrato {
-    
+
     private static Integer counter = 0;
-    
+
     private Element MainElement;
-    
+
     public Contrato(HashMap<String, String> hashMap) {
-        
+
         MainElement = new Element("contrato");
         MainElement.setAttribute("id", "id" + getId());
-        
+
         for (int i = 0; i < Contratos.strings.length; i++) {
-            
+
             String str = Contratos.strings[i];
             Element temp = new Element(str);
-            temp.setText(hashMap.get(str));
+            if (str.equals("preco")) {
+                
+                String precoString = hashMap.get(str);
+                precoString = precoString.replace(".", "").replace(",", ".");
+                temp.setText(precoString);
+            } else {
+                temp.setText(hashMap.get(str));
+            }
             MainElement.addContent(temp);
         }
     }
-    
+
     public Element getElement() {
         return MainElement;
     }
-    
+
     private synchronized String getId() {
         return (++counter).toString();
     }
